5eede611a35032a2eb948e6e4c97519ab5944258
Bogdan Zlobin <bogdan.tkachev@tptlive.ee>
2025-09-12 11:32:39 +0300

    Add traits example

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 class Box {
-    public $width;
+    private $width;
     public $height;
     public $lenght;
 
@@ -10,9 +10,15 @@ class Box {
     }
 }
 
+
+
 class MetalBox extends Box {
     public $material = 'Metal';
     public $weightPerUnit = 2;
+
+    public function test() {
+        var_dump($this->lenght);
+    }
 
     public function mass() {
         return $this->volume() * $this->weightPerUnit;
@@ -20,10 +26,10 @@ class MetalBox extends Box {
 }
 
 $metalBox1 = new MetalBox();
-$metalBox1->weight = 10;
-$metalBox1->height = 20;
-$metalBox1->lenght = 30;
+$metalBox1->setWidth = 10;
+//$metalBox1->test();
+//$metalBox1->height = 20;
+//$metalBox1->lenght = 30;
 
 var_dump($metalBox1->volume());
-var_dump($metalBox1->mass());
 var_dump($metalBox1);
